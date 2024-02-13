2336422ba83d3bd8a7dce5ccfd0814064275644f
nelsonj1614 <158496054+nelsonj1614@users.noreply.github.com>
2024-02-12 21:31:19 -0500

    added app code

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import pandas as pd 
+import streamlit as st 
+import plotly.express as px 
 
+df = pd.read_csv('vehicles_us.csv')
+
+st.header('This is my fabulous header!',divider='violet')
+
+data = df[df['price'] <= 100000]
+hist_data = data['price']
+histogram = px.histogram(hist_data)
+
+st.plotly_chart(histogram)
+
+scatter_data = df.dropna(subset='model_year')
+dependent = scatter_data['price']
+independent = scatter_data['model_year']
+scatter_plot = px.scatter(x=independent,y=dependent)
+
+st.plotly_chart(scatter_plot)
+
+# st.write()
+# st.checkbox()
